b7d62760a82cf6e367c0458f31491a730ff15bd8
wc-kunishi <110653494+wc-kunishi@users.noreply.github.com>
2022-08-08 15:11:35 +0900

    main panel done

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <main>
     <div class="main-panel-out">
-      <img src="~assets/images/index_main.png" alt="" id="absolute-image" class="z-index-2">
-      <div class="main-panel z-index-1">
-      </div>
       <div class="main-contents">
         <div class="main-text-area">
           <p class="main-text24">あなたの隣に這いよるナビAIを作る</p>
@@ -13,7 +10,16 @@
           <img src="~assets/images/index_main1.png" alt="" width="306" height="483">
         </div>
       </div>
+    </div>
 
+    <div>
+      <div class="content-title">
+        <p>なぜナビゲーションAIなのか</p>
+      </div>
+
+      <div>
+
+      </div>
     </div>
 
 
